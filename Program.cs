9172d725117ef752bc74b67420f9255850ca63b7
KAMichels <example@mail.com>
2024-11-10 13:31:29 +0200

    Week 14

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Dog goodBoy = new Dog("Good Boy");
+Console.WriteLine($"Name: {goodBoy.Name}; Level of happiness: {goodBoy.LevelOfHappiness}");
+
+while(goodBoy.LevelOfHappiness < 5)
+{
+    goodBoy.Bark();
+}
+
+goodBoy.Wag();
+class Dog
+{
+    public string Name;
+    public int LevelOfHappiness;
+
+    public Dog(string name)
+    {
+        Name = name;
+        LevelOfHappiness = 0;
+        Console.WriteLine($"{Name} is your friend now.");
+
+    }
+    public void Bark()
+    {
+        LevelOfHappiness++;
+        Console.WriteLine("Woof-Woof!");
+    }
+
+    public void Wag()
+    {
+        Console.WriteLine($"{Name} starts wagging it's tail.");
+        Console.WriteLine("Whoosh-Whoosh");
+    }
+}
